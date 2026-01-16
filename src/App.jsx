5127188051d6df3a276bf95d2339984196c5ae68
vishaldewani206 <vishaldewani500@gmail.com>
2026-01-16 19:10:05 +0500

    Implement first video play on scroll section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap"
 import { ScrollTrigger } from "gsap/all"
 import {Hero} from "./sections/Hero"
 import {Navbar} from "./sections/Navbar"
+import { FirstVideo } from "./sections/FirstVideo"
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -11,6 +12,7 @@ function App() {
     <main>
       <Navbar />
       <Hero />
+      <FirstVideo />
     </main>
   )
 }
