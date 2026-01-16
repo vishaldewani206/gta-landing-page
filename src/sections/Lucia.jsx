a1b224e92fb6ebc1eacea56f17df7bb4b0080fca
vishaldewani206 <vishaldewani500@gmail.com>
2026-01-16 20:37:28 +0500

    last touch

--- src/sections/Lucia.jsx
+++ src/sections/Lucia.jsx
@@ -1,11 +1,8 @@
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 export const Lucia = () => {
-
-  useGSAP(()=>{
-
-    gsap.set('.lucia-life', {marginTop: '-80vh'})
+  useGSAP(() => {gsap.set('.lucia-life', { marginTop: '-80vh'});
 
     gsap.timeline({
       scrollTrigger: {
@@ -14,7 +11,7 @@ export const Lucia = () => {
         end: '10% center',
         scrub: 2,
       }
-    }).to('.second-vd', {opacity: 0, duration: 1, ease: 'power1.inOut'})
+    }).to('.second-vd', { opacity: 0, duration: 1, ease: 'power1.inOut' });
 
     gsap.to('.lucia-life .img-box', {
       scrollTrigger: {
@@ -22,46 +19,36 @@ export const Lucia = () => {
         start: 'top center',
         end: '80% center',
         scrub: 2
-      },
+      }, 
       y: -200, 
-      duration: 1,
+      duration: 1, 
       ease: 'power1.inOut'
     }, '<')
-
-  },[])
+  });
 
   return (
-    <section className='lucia-life'>
-      <div className='flex flex-col gap-5 items-end img-box lg:w-1/2 ps-10 mt-96 '>
-        <div className='lucia-1'>
-          <img src="/images/lucia-1.webp" alt="" />
+    <section className="lucia-life">
+      <div className="flex flex-col gap-5 items-end img-box lg:1/2 ps-10 mt-96">
+        <div className="lucia-1">
+          <img src="/images/lucia-1.webp" />
         </div>
-        <div className='lucia-3'>
-          <img src="/images/lucia-3.webp" alt="" />
+        <div className="lucia-3">
+          <img src="/images/lucia-3.webp" />
         </div>
       </div>
 
-      <div className='lg:w-1/2 lucia-life-content'>
-        <div className='max-w-xl lg:ps-32 ps-10'>
+      <div className="lg:w-1/2 lucia-life-content">
+        <div className="max-w-xl lg:ps-32 ps-10">
           <h1>Lucia Caminos</h1>
-          <h1>Lucia's father taught her to fight as soon as she could walk</h1>
-          <h2>A life with Jason could be her way out.</h2>
-          <p>
-            Life has been coming at her swinging ever since. Fighting for her family landed 
-            her in the Leonida Penitentiary. Sheer luck got her out. Lucia's learned her lesson
-            -only smart moves from here
-          </p>
+          <h2>Lucia’s father taught her to fight as soon as she could walk.</h2>
+          <p>Life has been coming at her swinging ever since. Fighting for her family landed her in the Leonida Penitentiary. Sheer luck got her out. Lucia’s learned her lesson — only smart moves from here.</p>
         </div>
 
-        <div className='lucia-2'>
-          <img src="/images/lucia-2.webp" alt="" />
+        <div className="lucia-2">
+          <img src="/images/lucia-2.webp" />
         </div>
 
-        <p className='max-w-xl lg:ps-32 ps-10'>
-          More than anything, Lucia wants the good life her mom has dreamed of 
-          since their days in Liberty City - but instead of half-baked fantasies,
-          Lucia is prepared to take matters into her own hands.
-        </p>
+        <p className="max-w-xl lg:ps-32 ps-10">More than anything, Lucia wants the good life her mom has dreamed of since their days in Liberty City — but instead of half-baked fantasies, Lucia is prepared to take matters into her own hands.</p>
       </div>
     </section>
   )
